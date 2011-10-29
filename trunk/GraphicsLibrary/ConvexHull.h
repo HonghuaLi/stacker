c9dea49860305d540409cda1d09348d130547959
Howard.hhLi@gmail.com <Howard.hhLi@gmail.com@f3b1d4b8-2d33-0148-cb6f-9ea55fc52c2c>
2011-10-29 06:47:21 +0000

    Modified ConvexHull

--- trunk/GraphicsLibrary/ConvexHull.h
+++ trunk/GraphicsLibrary/ConvexHull.h
@@ -1,6 +1,10 @@
 // From Geometric Tools, LLC
 #pragma once
 
+//Visulization
+#include "Surface_mesh.h"
+#include "SimpleDraw.h"
+
 #include "SurfaceMesh/Vector.h"
 #include <vector>
 #include <set>
@@ -9,24 +13,22 @@
 
 typedef double								Real;
 typedef Vector<Real, 3>						Vector3;
-typedef std::vector<Vector3>::iterator		InputIterator;
 
 class  ConvexHull
 {
 public:
-    // The input to the constructor is a container of vertices whose convex hull
-    // is required.  
-    ConvexHull (InputIterator begin, InputIterator end);
+	ConvexHull(std::vector<Vector3> &pnts);
+	ConvexHull(Surface_mesh * mesh);
+    void computeCH();
+	void draw();
 
 private:
-
-	std::vector<InputIterator> getExtremes( InputIterator begin, InputIterator end, bool *CCW);
 	class TriFace
 	{
 	public:
 		TriFace (int v0, int v1, int v2);
 
-		int GetSign (InputIterator v);
+		int GetSign( int id , std::vector<Vector3> &pnts);
 		void AttachTo (TriFace* adj0, TriFace* adj1, TriFace* adj2);
 		int DetachFrom (int adjIndex, TriFace* adj);
 
@@ -36,13 +38,6 @@ private:
 		int Time;
 		bool OnStack;
 	};
-
-    bool Update (InputIterator v);
-    void ExtractIndices ();
-    void DeleteHull ();
-
-    // The current hull.
-    std::set<TriFace*> mHull;
 
     class TerminatorData
     {
@@ -54,4 +49,17 @@ private:
         int NullIndex;
         TriFace* T;
     };
+
+private:
+	bool getExtremes( std::vector<int> &mExtremes);
+	bool Update (int id);
+	void ExtractIndices ();
+	void DeleteHull ();
+
+private:
+	std::vector<Vector3> mPnts;	// The input pnts
+	std::set<TriFace*> mHull;		// The current hull.
+	int mNumSimplices;
+	std::vector<int> mIndices;
+	bool isReady;
 };
