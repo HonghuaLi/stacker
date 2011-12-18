80fa57b416c362b4fc52b6d7bb7a8dcd4e48e727
ennetws <ennetws@f3b1d4b8-2d33-0148-cb6f-9ea55fc52c2c>
2011-12-18 20:48:50 +0000

    Coplanar group code added.

--- trunk/Stacker/CoplanarGroup.cpp
+++ trunk/Stacker/CoplanarGroup.cpp
@@ -31,18 +31,37 @@ void CoplanarGroup::draw()
 
 		foreach(std::vector<Vec3d> curve, curves)
 		{
-			Vec3d v = cross(curve[0], curve[1]).normalized();
+			// find curve center
+			Vec3d curveCenter(0,0,0);
+			foreach(Point p, curve)	curveCenter += p;
+			curveCenter /= curve.size();
+
+			Vec3d curveAxis = cross(curve[0] - curveCenter, curve[1] - curveCenter).normalized();
 
 			int c = 0;
 
-			if(1 - abs(dot(v, groupAxis[0].n)) < EPS)	c = 1;
-			if(1 - abs(dot(v, groupAxis[1].n)) < EPS)	c = 2;
+			double d1 = abs(dot(curveAxis, groupAxis[0].n));
+			double d2 = abs(dot(curveAxis, groupAxis[1].n));
+			double d3 = abs(dot(curveAxis, groupAxis[2].n));
 
-			SimpleDraw::IdentifyCurve(curve, color[c][0], color[c][1], color[c][2], 1.0, 20);
+			if(d1 > d2) c = 1;
+			if(d2 > d3) c = 2;
+
+			for(uint k = 0; k < curve.size(); k++)
+			{
+				Vec3d delta = curveAxis * 0.01;
+
+				curve[k] += delta;
+			}
+
+			// Draw as closed loop
+			curve.push_back(curve.front());
+			SimpleDraw::IdentifyCurve(curve, color[c][0], color[c][1], color[c][2], 1.0, 15);
 		}
 	}
 
-	groupAxis[0].draw();
-	groupAxis[1].draw();
-	groupAxis[2].draw();
+	// Debug:
+	//groupAxis[0].draw();
+	//groupAxis[1].draw();
+	//groupAxis[2].draw();
 }
