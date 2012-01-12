56cc89e335f87bcb282a4e3728d2a9c316f4fcfb
Howard.hhLi <Howard.hhLi@gmail.com>
2012-01-12 06:08:50 +0000

    Added global.h/.cpp

---
+++ GUI/global.cpp
@@ -0,0 +1,10 @@
+#include "global.h"
+
+
+bool NORMALIZE_MESH = true;
+bool MOVE_CENTER_TO_ORIGIN = false;
+
+int NUM_EXPECTED_SOLUTION = 10;
+double BB_TOLERANCE = 1.1;
+double TARGET_STACKABILITY = 0.7;
+double HOT_RANGE = 0.95;
