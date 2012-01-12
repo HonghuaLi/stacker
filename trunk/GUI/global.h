868fb0330c4a926a1ec2f78e173b679b0e6d56ab
Howard.hhLi@gmail.com <Howard.hhLi@gmail.com@f3b1d4b8-2d33-0148-cb6f-9ea55fc52c2c>
2012-01-12 06:08:50 +0000

    Added global.h/.cpp

---
+++ trunk/GUI/global.h
@@ -0,0 +1,10 @@
+#pragma once
+
+
+extern bool NORMALIZE_MESH;
+extern bool MOVE_CENTER_TO_ORIGIN;
+
+extern int NUM_EXPECTED_SOLUTION;
+extern double BB_TOLERANCE;
+extern double TARGET_STACKABILITY;
+extern double HOT_RANGE;
