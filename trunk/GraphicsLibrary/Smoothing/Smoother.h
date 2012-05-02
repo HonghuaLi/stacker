c939f16008e4d2adf10c7cd47d19ed7ff61fa28f
ennetws <ennetws@f3b1d4b8-2d33-0148-cb6f-9ea55fc52c2c>
2012-05-02 17:24:56 +0000

    Code for smoothing. Still not yet functional.

--- trunk/GraphicsLibrary/Smoothing/Smoother.h
+++ trunk/GraphicsLibrary/Smoothing/Smoother.h
@@ -1,31 +1,5 @@
 #pragma once 
 
-// Linear Solver ================
-// WARNING: disabled SparseLib++ since we aren't using it in this project
-/*#undef Vector
-
-#define COMPLEX std::complex<double>
-
-#include "compcol_double.h"
-#include "mvblasd.h"
-
-// Pre conditioners
-#include "diagpre_double.h"
-#include "icpre_double.h"
-#include "ilupre_double.h"
-
-// Solvers
-#include "bicg.h"
-#include "bicgstab.h"
-#include "cg.h"
-#include "cgs.h"
-
-#define Vector std::vector*/
-// End of Solver ================
-
-#undef min
-#undef max
-
 #include "GraphicsLibrary/Mesh/QSurfaceMesh.h"
 
 class Smoother{
@@ -40,6 +14,6 @@ public:
 	static void ScaleDependentSmoothing(Surface_mesh * m, int numIteration, double step_size = 0.5, bool protectBorders = true);
 	static Point ScaleDependentSmoothVertex(Surface_mesh * m, Surface_mesh::Vertex v, double step_size = 0.5);
 
-	static void MeanCurvatureFlow(QSurfaceMesh * m, double step, int numIteration, bool isVolumePreservation = true);
+	static void MeanCurvatureFlow(QSurfaceMesh * m, int numIteration = 1, double step = 0.25, bool isVolumePreservation = true);
 	static void MeanCurvatureFlowExplicit(QSurfaceMesh * m, int numIteration, double step = 0.5);
 };
